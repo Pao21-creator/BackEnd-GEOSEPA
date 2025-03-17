c78a8be39075fb94958e6932c43c8a3a2754d0c0
Pao21-creator <mendozapaolagus@gmail.com>
2025-03-17 10:44:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const NodeCache = require('node-cache');  // Importamos node-cache
 const { PoligonEarthEngine } = require('./EngineServicePolygon');
 const { graficoAnualCuenca } = require('./cuencas/serviceCuencaAnual');
 const { PointEarthEngine } = require('./EngineServicePoint');
@@ -12,6 +13,9 @@ const { graficoNdviPoligono } = require('./ndvi/serviceNdviPoly');
 
 const app = express();
 const port = 3000;
+
+// Crear una instancia del caché
+const myCache = new NodeCache({ stdTTL: 60 * 60, checkperiod: 120 });  // TTL de 1 hora (en segundos)
 
 // Configurar CORS para permitir encabezados como Content-Type
 app.use(cors({
@@ -29,30 +33,44 @@ const handleError = (res, error) => {
   res.status(500).json({ error: error.message || 'Error desconocido' });
 };
 
-// Función centralizada para procesar solicitudes
-const processRequest = async (req, res, callback) => {
+// Función para procesar solicitudes con caché
+const processRequestWithCache = async (req, res, callback, cacheKey) => {
+  const cachedResult = myCache.get(cacheKey);
+  if (cachedResult) {
+    console.log('Respuesta desde caché');
+    return res.json(cachedResult);
+  }
+
   try {
+    if (typeof callback !== 'function') {
+      throw new Error('Callback proporcionado no es una función');
+    }
     const result = await callback(req.body);
+    myCache.set(cacheKey, result);
     res.json(result);
   } catch (error) {
     handleError(res, error);
   }
 };
 
+
 // Rutas para Earth Engine
 app.post('/dataPolygonNdvi', (req, res) => {
-  processRequest(req, res, PoligonEarthEngine);
+  const cacheKey = `dataPolygonNdvi-${JSON.stringify(req.body)}`;  // Usamos el cuerpo de la solicitud como clave
+  processRequestWithCache(req, res, PoligonEarthEngine, cacheKey);
 });
 
 app.post('/dataPointNDVI', (req, res) => {
-  processRequest(req, res, PointEarthEngine);
+  const cacheKey = `dataPointNDVI-${JSON.stringify(req.body)}`;
+  processRequestWithCache(req, res, PointEarthEngine, cacheKey);
 });
 
 // Rutas para gráficos y análisis de cuencas
 app.post('/getMapIdCuenca', async (req, res) => {
   const { cuenca, año, funcion } = req.body;
   if (funcion === 'graficoAnual') {
-    processRequest(req, res, () => graficoAnualCuenca(cuenca, Number(año)));
+    const cacheKey = `graficoAnualCuenca-${cuenca}-${año}`;
+    processRequestWithCache(req, res, () => graficoAnualCuenca(cuenca, Number(año)), cacheKey);
   } else {
     res.status(400).json({ error: 'Función no válida' });
   }
@@ -60,12 +78,11 @@ app.post('/getMapIdCuenca', async (req, res) => {
 
 app.post('/getCuencaYearComparador', async (req, res) => {
   const { cuenca, año, funcion } = req.body;
-  switch (funcion) {
-    case 'graficoComparativo':
-      processRequest(req, res, () => graficoAnual2Años(cuenca, Number(año)));
-      break;
-    default:
-      res.status(400).json({ error: 'Función no válida' });
+  if (funcion === 'graficoComparativo') {
+    const cacheKey = `graficoComparativo-${cuenca}-${año}`;
+    processRequestWithCache(req, res, () => graficoAnual2Años(cuenca, Number(año)), cacheKey);
+  } else {
+    res.status(400).json({ error: 'Función no válida' });
   }
 });
 
@@ -73,7 +90,8 @@ app.post('/getCuencaYearComparador', async (req, res) => {
 app.post('/getMapIdNdvi', async (req, res) => {
   const { año, funcion, point, prov } = req.body;
   if (funcion === 'graficoAnual') {
-    processRequest(req, res, () => graficoNdviProv(prov, Number(año), point));
+    const cacheKey = `graficoNdviProv-${prov}-${año}-${point}`;
+    processRequestWithCache(req, res, () => graficoNdviProv(prov, Number(año), point), cacheKey);
   } else {
     res.status(400).json({ error: 'Función no válida' });
   }
@@ -81,20 +99,20 @@ app.post('/getMapIdNdvi', async (req, res) => {
 
 app.post('/getNdviYearComparador', async (req, res) => {
   const { año, funcion, point, prov } = req.body;
-  console.log("provincia es",prov)
   if (funcion === 'graficoComparativo') {
-    processRequest(req, res, () => graficoNdviPunto(point, Number(año)));
+    const cacheKey = `graficoNdviPunto-${point}-${año}`;
+    processRequestWithCache(req, res, () => graficoNdviPunto(point, Number(año)), cacheKey);
   } else {
     res.status(400).json({ error: 'Función no válida' });
   }
 });
 
-
 app.post('/getPointNdvi', async (req, res) => {
   const { año, funcion, point } = req.body;
 
   if (funcion === 'graficoAnual') {
-    processRequest(req, res, () => graficoNdviPunto(point, Number(año)));
+    const cacheKey = `graficoNdviPunto-${point}-${año}`;
+    processRequestWithCache(req, res, () => graficoNdviPunto(point, Number(año)), cacheKey);
   } else {
     res.status(400).json({ error: 'Función no válida' });
   }
@@ -104,12 +122,12 @@ app.post('/getPolyNdvi', async (req, res) => {
   const { año, funcion, polygon } = req.body;
 
   if (funcion === 'graficoAnual') {
-    processRequest(req, res, () => graficoNdviPoligono(polygon, Number(año)));
+    const cacheKey = `graficoNdviPoligno-${polygon}-${año}`;
+    processRequestWithCache(req, res, () => graficoNdviPoligono(polygon, Number(año)), cacheKey);
   } else {
     res.status(400).json({ error: 'Función no válida' });
   }
 });
-
 
 // Ruta para obtener promedio
 app.get('/promedio', async (req, res) => {
